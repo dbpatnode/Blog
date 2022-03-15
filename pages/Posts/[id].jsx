cea02ea5c694bdd16d66d460b46d213807a4a814
Daniel Patnode <danielpatnode@Daniels-MacBook-Pro.local>
2022-03-14 17:05:41 -0700

    publishing a blog successful

--- pages/Posts/[id].jsx
+++ pages/Posts/[id].jsx
@@ -1,41 +1,59 @@
-import { supabase } from '../../utils/supabaseClient'
+import { redirect } from "next/dist/server/api-utils";
+import { useRouter } from "next/router";
+import { supabase } from "../../utils/supabaseClient";
+import Posts from "./index";
 
 export async function getServerSideProps({ params }) {
-    const { data: post, error } = await supabase
-    .from('posts')
-    .select('*')
-    .eq('id', params.id)
-    .single()
+  const { data: post, error } = await supabase
+    .from("posts")
+    .select("*")
+    .eq("id", params.id)
+    .single();
 
-    if(error) {
-        alert(error.message)
-    }
-    return {
-        props: {
-            post
-        }
-    }
+  if (error) {
+    alert(error.message);
+  }
+  return {
+    props: {
+      post,
+    },
+  };
 }
 
-export async function deletePost(id) {
- console.log("we're here!")
+export async function updatePost(id, column) {
+  const { data, error } = await supabase
+    .from("posts")
+    .update({ is_published: true })
+    .eq("id", id);
 
- const { data, error } = await supabase
-  .from('posts')
-  .delete()
-  .eq('id', id)
+  console.log(error);
 }
 
-export default function PostPage ({ post }) {
-    const {title, content, id} = post
-    return (
-        <div key={id}>
-            <h2>{title}</h2>
-            <p>{content}</p>
-            <button
-            type="button"
-            onClick={() => deletePost(id)}>Delete</button>
-            {/* <button>Edit</button> */}
-        </div>    
-    )
+export async function deletePost(id, router) {
+  const { data, error } = await supabase.from("posts").delete().eq("id", id);
+  return router.push("/Posts");
+}
+
+export default function PostPage({ post }) {
+  const router = useRouter();
+  const { title, content, id } = post;
+  return (
+    <div key={id}>
+      <h2>{title}</h2>
+      <p>{content}</p>
+      <button
+        type="button"
+        onClick={() => {
+          window.confirm("Are you sure you wish to delete this item?")
+            ? deletePost(id, router)
+            : onCancel("cancel");
+        }}
+      >
+        Delete
+      </button>
+      <button type="button" onClick={() => updatePost(id, "is_published")}>
+        Publish
+      </button>
+    </div>
+  );
 }
--- pages/Posts/index.js
+++ pages/Posts/index.js
@@ -1,32 +1,51 @@
 // import { useState} from 'react'
 // import { getPosts } from '../../lib/Posts'
 // import { Spinner } from '../../components/Spinner'
-import PostPreview from '../../components/PostPreview'
-import styles from '../../styles/Home.module.css'
-import { supabase } from '../../utils/supabaseClient'
-
+import { useRouter } from "next/router";
+import PostPreview from "../../components/PostPreview";
+import styles from "../../styles/Home.module.css";
+import { supabase } from "../../utils/supabaseClient";
 
 export async function getStaticProps() {
- 
-    const { data: posts, error } = await supabase
-    .from('posts')
-    .select('*')
+  const { data: posts, error } = await supabase.from("posts").select("*");
 
-    if(error) {
-        alert(error.message)
-    }
-    return {
-        props: {
-            posts
-        }
-    }
+  if (error) {
+    alert(error.message);
+  }
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+const defaultPost = {
+  title: "default title",
+  is_published: false,
+  content: "this is some content",
+  // slug: "",
+  excerpt: "dsajfksadjhf",
+};
+
+export async function createPost(router) {
+  const { data, error } = await supabase.from("posts").insert(defaultPost);
+
+  if (data[0].id) {
+    router.push(`/Posts/${data[0].id}`);
+  }
 }
 
 export default function Posts({ posts }) {
-    return (    
-        <div className={styles.container}>
-            <h1>Published Blogs</h1>
-            <PostPreview posts={posts}/>
-        </div>
-    )
+  const router = useRouter();
+
+  return (
+    <div className={styles.container}>
+      <h1>Published Blogs</h1>
+      <PostPreview posts={posts} />
+      <button type="button" onClick={() => createPost(router)}>
+        {" "}
+        Create New Post{" "}
+      </button>
+    </div>
+  );
 }
