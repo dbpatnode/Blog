06fc9978adaed4bda1fb4e7356aabb5f44b20e08
Daniel Patnode <dbpatnode@gmail.com>
2022-03-18 10:46:23 -0700

    restyled cards

--- components/PostPreview.js
+++ components/PostPreview.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const PostPreview = ({ posts }) => {
   return (
-    <div className="Posts">
+    <div className="card-container">
       {posts?.map((post) => {
         const { is_published, title, excerpt, id } = post;
 
@@ -15,27 +15,24 @@ const PostPreview = ({ posts }) => {
             //   <p> {excerpt} </p>
             // </div>
 
-            <div class="container">
-              <div class="card">
-                <div class="card-header">
-                  <img
-                    src="https://c0.wallpaperflare.com/preview/483/210/436/car-green-4x4-jeep.jpg"
-                    alt="no-image-found"
-                  />
-                </div>
-                <div class="card-body">
-                  <span class="tag tag-teal">Technology</span>
-                  <h4>
-                    <Link href={`/Posts/${id}`}>{title}</Link>
-                  </h4>
-                  <p>{excerpt}</p>
-                  <div class="user-info">
-                    <h5>July Dec</h5>
-                    <small>3h ago</small>
-                  </div>
+            // <div class="container">
+            <div class="card">
+              <div class="card-header">
+                <img src="./images/no-image.jpeg" alt="no-image-found" />
+              </div>
+              <div class="card-body">
+                <span class="tag tag-teal">Technology</span>
+                <h4>
+                  <Link href={`/Posts/${id}`}>{title}</Link>
+                </h4>
+                <p>{excerpt}</p>
+                <div class="user-info">
+                  <h5>July Dec</h5>
+                  <small>3h ago</small>
                 </div>
               </div>
             </div>
+            // </div>
           )
         );
       })}
