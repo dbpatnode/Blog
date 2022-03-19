02f8682a76a7178b3007bac8fd066f74f2dc6ba3
Daniel Patnode <dbpatnode@gmail.com>
2022-03-18 22:34:40 -0700

    FAQ page setup

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,49 @@
-export default function about() {
-    return (
-        <div>
-            <h1> About Page </h1>
-            <p> A sweet little blog site.</p>
-            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJiIYYM8KuiWkiai8UolvXvoN7A1UF5cmboA&usqp=CAU' alt='Rambo thumbs up'></img>
-        </div>
+// import { useEffect, useState } from "react";
+import Faq from "react-faq-component";
 
-    )
+const data = {
+  title: "FAQ (How it works)",
+  rows: [
+    {
+      title: "Lorem ipsum dolor sit amet,",
+      content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed tempor sem. Aenean vel turpis feugiat,
+              ultricies metus at, consequat velit. Curabitur est nibh, varius in tellus nec, mattis pulvinar metus.
+              In maximus cursus lorem, nec laoreet velit eleifend vel. Ut aliquet mauris tortor, sed egestas libero interdum vitae.
+              Fusce sed commodo purus, at tempus turpis.`,
+    },
+    {
+      title: "Nunc maximus, magna at ultricies elementum",
+      content:
+        "Nunc maximus, magna at ultricies elementum, risus turpis vulputate quam, vitae convallis ex tortor sed dolor.",
+    },
+    {
+      title: "Curabitur laoreet, mauris vel blandit fringilla",
+      content: `Curabitur laoreet, mauris vel blandit fringilla, leo elit rhoncus nunc, ac sagittis leo elit vel lorem.
+            Fusce tempor lacus ut libero posuere viverra. Nunc velit dolor, tincidunt at varius vel, laoreet vel quam.
+            Sed dolor urna, lobortis in arcu auctor, tincidunt mattis ante. Vivamus venenatis ultricies nibh in volutpat.
+            Cras eu metus quis leo vestibulum feugiat nec sagittis lacus.Mauris vulputate arcu sed massa euismod dignissim. `,
+    },
+  ],
+};
+
+const styles = {
+  // bgColor: 'white',
+  titleTextColor: "black",
+  rowTitleColor: "black",
+  // rowContentColor: 'grey',
+  // arrowColor: "red",
+};
+
+const config = {
+  // animate: true,
+  // arrowIcon: "V",
+  // tabFocus: true
+};
+
+export default function About() {
+  return (
+    <div>
+      <Faq data={data} styles={styles} config={config} />
+    </div>
+  );
 }
