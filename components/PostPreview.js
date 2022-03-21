e81db521fdb8f80ea6b51564a583be49b2eb9f95
Daniel Patnode <dbpatnode@gmail.com>
2022-03-20 19:50:30 -0700

    changed icons on card.

--- components/PostPreview.js
+++ components/PostPreview.js
@@ -1,8 +1,24 @@
 import dayjs from "dayjs";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCoffee,
+  faPencilSquare,
+  faTrash,
+  faTrashAlt,
+} from "@fortawesome/free-solid-svg-icons";
+import { useRouter } from "next/router";
+import { supabase } from "../utils/supabaseClient";
 import timeSincePosted from "./TimeSincePosted";
 import Link from "next/link";
+import Button from "./Button";
+
+export async function deletePost(id, router) {
+  const { data, error } = await supabase.from("posts").delete().eq("id", id);
+  return router.push("/Posts");
+}
 
 const PostPreview = ({ posts }) => {
+  const router = useRouter();
   return (
     <div className="card-container">
       {posts?.map((post) => {
@@ -21,6 +37,29 @@ const PostPreview = ({ posts }) => {
                   <Link href={`/Posts/${id}`}>{title}</Link>
                 </h4>
                 <p>{excerpt}</p>
+                <div class="card-buttons">
+                  <Button
+                    className="edit-icon icon-btn"
+                    handleClick={() => {
+                      /* this is where we'll add the edit*/ console.log(
+                        "this is where we'll add the edit"
+                      );
+                    }}
+                    text={<FontAwesomeIcon icon={faPencilSquare} size="2x" />}
+                  />
+
+                  <Button
+                    className="trash-icon icon-btn"
+                    handleClick={() => {
+                      window.confirm(
+                        "Are you sure you wish to delete this item?"
+                      )
+                        ? deletePost(id, router)
+                        : "cancel";
+                    }}
+                    text={<FontAwesomeIcon icon={faTrashAlt} size="2x" />}
+                  />
+                </div>
                 <div className="user-info">
                   <h5>Posted on {formatedDate}</h5>
                   <small>{timeSincePosted(created_at)} </small>
